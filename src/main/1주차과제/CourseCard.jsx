f28b7c568141018d95c24e9af27733d2e809cd08
choiSangHyeop <xoxoxmin1268@naver.com>
2025-05-02 12:08:40 +0900

    one week assignment commit

---
+++ src/main/1주차과제/CourseCard.jsx
@@ -0,0 +1,15 @@
+import './CourseCard.css';
+
+function CourseCard({
+        img,
+        tags,
+        title,
+        startPrice,
+        types
+    }) {
+        return (
+            <div className="CourseCard"></div>
+        )
+    }
+
+export default CourseCard;
