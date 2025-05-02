9a68d3c964108b9477df328f3c8c88409a265313
saysuhyun <saysuhyun@gmail.com>
2025-05-03 08:47:40 +0900

    1주차 - 최수현

--- suhyun-project/src/App.tsx
+++ suhyun-project/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Virtual Dom 예시
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount((prev) => prev + 1);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={increase}>Increase</button>
+      <Counter count={count} />
+      <Info />
+    </div>
+  );
+};
 
-export default App
+export default App;
