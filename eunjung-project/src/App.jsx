7cc80f64a289bd809ef84910cd82829a406ec721
unknown <sogoal17@naver.com>
2025-05-06 09:25:03 -0500

    add file

--- eunjung-project/src/App.jsx
+++ eunjung-project/src/App.jsx
@@ -1,29 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-/** 
- * For First Study
-*/
- import Parent from './first_study/Parent.jsx'
- import NextPage from './first_study/NextPage.jsx'
-//import ParentCallBack from './first_study/ParentCallBack.jsx'
-//import LifeCycleDemo from './first_study/LifeCycleDemo.jsx'
-import TimerComponent from './first_study/TimerComponent.jsx'
-//import ClickEventListener from './first_study/ClickEventListener.jsx'
-//import LayoutVsEffect from './first_study/LayoutVsEffect.jsx'
+import FirstPage from './first_study/FirstPage';
+import Course from './1주차과제/Course';
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Parent/>} />
-        <Route path="/TimerComponent" element={<TimerComponent/>} />
-        <Route path="/next" element={<NextPage />} />
-      </Routes>
-    </BrowserRouter>
+      <div>
+        {/* <FirstPage></FirstPage> */}
+        <Course></Course>
+      </div>
     </>
   )
 }
